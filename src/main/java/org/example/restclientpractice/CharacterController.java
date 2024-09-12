03007422e60c1734ee6d22c5ea58a21f4af7e76d
marleneanton <the.marlene.anton@gmail.com>
2024-09-12 10:59:07 +0200

    creates get mapping to return all characters with limited fields

--- src/main/java/org/example/restclientpractice/CharacterController.java
+++ src/main/java/org/example/restclientpractice/CharacterController.java
@@ -1,15 +1,22 @@
 package org.example.restclientpractice;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/characters")
+@RequestMapping("/api/characters/")
 public class CharacterController {
 
     RestClient.Builder builder = RestClient.builder();
     private final CharacterAPIService characterAPIService = new CharacterAPIService(builder);
 
     // todo: implement get mapping that outputs json with those three attributes
+    @GetMapping
+    List<CharacterAPIResponseResults> getAllCharacters(){
+        return characterAPIService.getAllCharacters();
+    }
 }

8ab230d16e051a678c38a6cdbc48a2136eb6950a
marleneanton <the.marlene.anton@gmail.com>
2024-09-12 10:40:22 +0200

    Initial commit

---
+++ src/main/java/org/example/restclientpractice/RestClientPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.example.restclientpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestClientPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/restclientpractice/RestClientPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.restclientpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
